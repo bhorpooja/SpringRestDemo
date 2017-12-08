dbfb72c40e4848f469a1bdcb069feaab5cfb2a04
pooja <bhorpooja222gmail.com>
2017-12-08 15:00:22 +0530

    Give @DBRef

---
+++ src/main/java/com/codekul/SpringRestDemo/domain/Dept.java
@@ -0,0 +1,7 @@
+package com.codekul.SpringRestDemo.domain;
+
+/**
+ * Created by pooja on 8/12/17.
+ */
+public class Dept {
+}
---
+++ src/main/java/com/codekul/SpringRestDemo/repo/DeptRepo.java
@@ -0,0 +1,7 @@
+package com.codekul.SpringRestDemo.repo;
+
+/**
+ * Created by pooja on 8/12/17.
+ */
+public interface DeptRepo {
+}
