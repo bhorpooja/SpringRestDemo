c5787501a5a5cb3e007564ea37aa82ae43df325e
pooja <bhorpooja222gmail.com>
2017-12-12 12:50:39 +0530

    REST Demo..

--- src/main/java/com/codekul/SpringRestDemo/repo/StudentRepo.java
+++ src/main/java/com/codekul/SpringRestDemo/repo/StudentRepo.java
@@ -2,7 +2,9 @@ package com.codekul.SpringRestDemo.repo;
 
 import com.codekul.SpringRestDemo.domain.Student;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 /**
  * Created by pooja on 8/12/17.
@@ -10,4 +12,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "student",path = "student")
 public interface StudentRepo extends MongoRepository<Student,String> {
+
+    @RestResource(path = "/findByName")
+    Student findByName(@Param("name") String name);
 }
